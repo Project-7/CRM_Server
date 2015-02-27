a73ad0d85d76e32d504e86a7aab215c0a93f7ca8
Robert Kálya <robert_ka1993@hotmail.de>
2015-02-27 11:27:16 +0100

    no message

--- src/CRM_Server_DB_Connection.java
+++ src/CRM_Server_DB_Connection.java
@@ -1,3 +1,9 @@
+
+import java.rmi.RemoteException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +15,22 @@
  * @author MarkusH
  */
 public class CRM_Server_DB_Connection {
+    
+    Connection con = null;
+    
+    public CRM_Server_DB_Connection() throws RemoteException, SQLException {
+        
+    }
+
+     public Connection getConnection() throws SQLException {
+         try {
+            System.out.println("Verbindung mit Datenbank wird hergestellt.");
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            System.err.println("Treiber konnte nicht gefunden werden."+e.getMessage());
+        }
+        return con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LAeRacing", "root", "0000");
+        
+     }
     
 }
